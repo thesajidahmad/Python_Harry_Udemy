caa1f5be610afc51b6690be63bb9396302d23156
thesajidahmad <101032376+thesajidahmad@users.noreply.github.com>
2026-01-06 22:54:44 +0530

    super() use in child

--- OOP/04_inheritance.py
+++ OOP/04_inheritance.py
@@ -13,16 +13,20 @@ class Animal:
         print(f"{self.name} have a mouth")
 
 class Dog(Animal):
+
     
     def dog_feature1(self):
+        super().feature1()
+        super().feature2()
+        super().feature3()
         print(f"{self.name} can Bark!!")
 
     def dog_feature2(self):
         print(f"{self.name} is a pet animal")
 
 d = Dog("Dogg")
-d.feature1()
-d.feature2()
-d.feature3()
+# d.feature1()
+# d.feature2()
+# d.feature3()
 d.dog_feature1()
 d.dog_feature2()
